702aa94d9479dcd2357ecab143017264eb8e68a8
fero <feroalien@hotmail.com>
2021-02-19 10:24:41 +1000

    update

--- src/Securing_Vue_js_with_IdentityServer/src/vue-app/vue.config.js
+++ src/Securing_Vue_js_with_IdentityServer/src/vue-app/vue.config.js
@@ -1,3 +1,13 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
-  outputDir: '../vueApi/wwwroot'
+  outputDir: '../vueApi/wwwroot',
+  configureWebpack:
+  {
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'node_modules/oidc-client/dist/oidc-client.min.js', to: 'js' }
+      ])
+    ]
+  }
 }
