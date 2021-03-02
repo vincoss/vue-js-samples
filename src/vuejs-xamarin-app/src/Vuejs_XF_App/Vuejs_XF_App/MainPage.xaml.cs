9705919ce1d671768ac020c2ed09bf3fe326fcdc
Ferdinand Lukasak <feroalien@hotmail.com>
2021-03-02 12:36:02 +1000

    http requests

--- src/vuejs-xamarin-app/src/Vuejs_XF_App/Vuejs_XF_App/MainPage.xaml.cs
+++ src/vuejs-xamarin-app/src/Vuejs_XF_App/Vuejs_XF_App/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Vuejs_XF_App.Views;
@@ -19,6 +20,45 @@ namespace Vuejs_XF_App
         private void Button_Clicked(object sender, EventArgs e)
         {
             this.Navigation.PushAsync(new AppWebView());
+        }
+
+        private async void httpGetValuesTest_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var result = await client.GetAsync("http://localhost:5000/api/values");
+                    result.EnsureSuccessStatusCode();
+                    var str = await result.Content.ReadAsStringAsync();
+                    lblInfo.Text = str;
+                }
+            }
+            catch(Exception ex)
+            {
+                lblInfo.Text = e.ToString();
+            }
+        }
+
+        private async void httpsGetValuesTest_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var clientHandler = new HttpClientHandler();
+                clientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => { return true; }; 
+
+                using (var client = new HttpClient(clientHandler))
+                {
+                    var result = await client.GetAsync("https://localhost:5001/api/values");
+                    result.EnsureSuccessStatusCode();
+                    var str = await result.Content.ReadAsStringAsync();
+                    lblInfo.Text = str;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblInfo.Text = e.ToString();
+            }
         }
     }
 }
