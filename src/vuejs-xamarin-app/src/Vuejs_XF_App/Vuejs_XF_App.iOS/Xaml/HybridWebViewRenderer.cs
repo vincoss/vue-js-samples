93405bb943af32c8a27b58a0a0fa5b2a00d68e16
fero <feroalien@hotmail.com>
2021-03-08 10:33:07 +1000

    iOS custom web view renderer

---
+++ src/vuejs-xamarin-app/src/Vuejs_XF_App/Vuejs_XF_App.iOS/Xaml/HybridWebViewRenderer.cs
@@ -0,0 +1,63 @@
+﻿using Foundation;
+using System.IO;
+using Vuejs_XF_App.iOS.Xaml;
+using Vuejs_XF_App.Xaml;
+using WebKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
+namespace Vuejs_XF_App.iOS.Xaml
+{
+    public class HybridWebViewRenderer : WkWebViewRenderer, IWKScriptMessageHandler
+    {
+        const string JavaScriptFunction = "function invokeCSharpAction(data){window.webkit.messageHandlers.invokeAction.postMessage(data);}";
+        WKUserContentController userController;
+
+        public HybridWebViewRenderer() : this(new WKWebViewConfiguration())
+        {
+        }
+
+        public HybridWebViewRenderer(WKWebViewConfiguration config) : base(config)
+        {
+            userController = config.UserContentController;
+            var script = new WKUserScript(new NSString(JavaScriptFunction), WKUserScriptInjectionTime.AtDocumentEnd, false);
+            userController.AddUserScript(script);
+            userController.AddScriptMessageHandler(this, "invokeAction");
+        }
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                userController.RemoveAllUserScripts();
+                userController.RemoveScriptMessageHandler("invokeAction");
+                HybridWebView hybridWebView = e.OldElement as HybridWebView;
+                hybridWebView.Cleanup();
+            }
+
+            if (e.NewElement != null)
+            {
+                string filename = Path.Combine(NSBundle.MainBundle.BundlePath, $"Resources/wwwroot/page0003{((HybridWebView)Element).Uri}");
+                LoadRequest(new NSUrlRequest(new NSUrl(filename, false)));
+            }
+        }
+
+        public void DidReceiveScriptMessage(WKUserContentController userContentController, WKScriptMessage message)
+        {
+            ((HybridWebView)Element).InvokeAction(message.Body.ToString());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ((HybridWebView)Element).Cleanup();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
