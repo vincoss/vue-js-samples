c161c617e3b30b6f2d95256880853ee18e4751dc
fero <feroalien@hotmail.com>
2021-11-23 11:12:04 +1000

    config sample

---
+++ src/hello-there/vue.config.js
@@ -0,0 +1,14 @@
+﻿// vue.config.js
+module.exports = {
+    transpileDependencies: ['vuetify'],
+    configureWebpack: config =>
+    {
+        if (process.env.NODE_ENV === 'production') {
+            // mutate config for production...
+        } else {
+            // mutate for development...
+        }
+    },
+    outputDir: '../hello-there/ClientApp/dist',
+    publicPath: ''
+}
