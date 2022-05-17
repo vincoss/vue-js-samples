2d7b46bba90b1bf70b9bf58a2eacd6b0b2a453b4
fero <feroalien@hotmail.com>
2022-05-17 15:22:45 +1000

    build fix

--- src/vuejs-xamarin-app/src/Vuejs_XF_App/Vuejs_XF_App.Android/Resources/Resource.designer.cs
+++ src/vuejs-xamarin-app/src/Vuejs_XF_App/Vuejs_XF_App.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace Vuejs_XF_App.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.153")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.99.43")]
 	public partial class Resource
 	{
 		
