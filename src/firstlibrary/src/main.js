f0f58696fad178d8e2561b311efc6dbac5045d4d
fero <feroalien@hotmail.com>
2022-06-08 09:48:58 +1000

    lib

--- src/firstlibrary/src/main.js
+++ src/firstlibrary/src/main.js
@@ -1,2 +1,6 @@
-﻿import DummyButton from './components/DummyButton.vue'
-export default DummyButton
+﻿/*
+ import DummyButton from './components/DummyButton.vue'
+export default DummyButton 
+*/
+import DataContextComponent from './components/DataContextComponent.vue'
+export default DataContextComponent 
