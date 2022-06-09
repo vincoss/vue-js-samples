0325b39963bba95c76d5033b70b5d4a16abd5ebd
Maabot <admin@maabot.com>
2022-06-09 14:30:58 +1000

    added sfc sample

---
+++ src/data-context-component/dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import DataContextComponent from '@/data-context-component.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    DataContextComponent
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <data-context-component />
+  </div>
+</template>
